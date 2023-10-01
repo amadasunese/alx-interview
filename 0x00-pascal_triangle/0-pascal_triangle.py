ee7bb8248beb02131ec9d993155f18f8f55d0248
Ese Amadasun <amadasunese@gmail.com>
2023-10-01 15:05:00 +0100

    pep updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,25 +2,27 @@
 """
 Function that returns Pascal’s triangle
 """
+
+
 def pascal_triangle(n):
     """
     Pascal triangle
     """
     if n <= 0:
         return []
-    
+
     # Pascal's Triangle first row.
     triangle = [[1]]
-    
+
     # Remaining rows.
     for i in range(1, n):
         row = [1]
         prev_row = triangle[i - 1]
-        
+
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
-        
+
         row.append(1)
         triangle.append(row)
-    
+
     return triangle
