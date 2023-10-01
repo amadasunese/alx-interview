a74f83537c43d592171989d15901f444806c6b9a
Ese Amadasun <amadasunese@gmail.com>
2023-10-01 14:01:11 +0100

    pascal triangle updated

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""function that returns a list of lists of Pascal’s triangle"""
+"""
+Function that returns Pascal’s triangle
+"""
 def pascal_triangle(n):
+    """
+    Pascal triangle
+    """
     if n <= 0:
         return []
     
-    # Initialize the Pascal's Triangle with the first row.
+    # Pascal's Triangle first row.
     triangle = [[1]]
     
-    # Generate the remaining rows.
+    # Remaining rows.
     for i in range(1, n):
-        row = [1]  # Each row starts with 1.
+        row = [1]
         prev_row = triangle[i - 1]
         
         for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])  # Calculate the next value.
+            row.append(prev_row[j - 1] + prev_row[j])
         
-        row.append(1)  # Each row ends with 1.
+        row.append(1)
         triangle.append(row)
     
     return triangle
