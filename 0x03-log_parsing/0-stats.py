d522a775aa86e5669ac3b03a7072b43f0c886e4a
Ese Amadasun <amadasunese@gmail.com>
2023-10-20 21:30:17 +0100

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,55 @@
 #!/usr/bin/python3
-"""Log Parser"""
+'''
+A script that reads stdin line by line
+'''
 import sys
+import re
 
 
-if __name__ == '__main__':
-    file_size = [0]
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                    403: 0, 404: 0, 405: 0, 500: 0}
+# Initialize variables to store metrics
+total_file_size = 0
+status_code_counts = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0
+}
+line_count = 0
 
-    def print_stats():
-        """ Print statistics """
-        print('File size: {}'.format(file_size[0]))
-        for key in sorted(status_codes.keys()):
-            if status_codes[key]:
-                print('{}: {}'.format(key, status_codes[key]))
+try:
+    for line in sys.stdin:
+        """
+        Parse the line using regular expressions
+        """
+        match = re.match(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^\]]+)] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$', line)
 
-    def parse_line(line):
-        """ Checks the line for matches """
-        try:
-            line = line[:-1]
-            word = line.split(' ')
-            # File size is last parameter on stdout
-            file_size[0] += int(word[-1])
-            # Status code comes before file size
-            status_code = int(word[-2])
-            # Move through dictionary of status codes
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except BaseException:
-            pass
+        if match:
+            ip_address, date, status_code, file_size = match.groups()
+            status_code = int(status_code)
+            file_size = int(file_size)
 
-    linenum = 1
-    try:
-        for line in sys.stdin:
-            parse_line(line)
-            """ print after every 10 lines """
-            if linenum % 10 == 0:
-                print_stats()
-            linenum += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+            # Update metrics
+            total_file_size += file_size
+            status_code_counts[status_code] += 1
+            line_count += 1
+
+            # Check if it's time to print statistics
+            if line_count % 10 == 0:
+                print(f"Total file size: {total_file_size}")
+                for code in sorted(status_code_counts.keys()):
+                    if status_code_counts[code] > 0:
+                        print(f"{code}: {status_code_counts[code]}")
+
+except KeyboardInterrupt:
+    # Handle CTRL+C interruption by printing statistics
+    print(f"Total file size: {total_file_size}")
+    for code in sorted(status_code_counts.keys()):
+        if status_code_counts[code] > 0:
+            print(f"{code}: {status_code_counts[code]}")
+
+except Exception as e:
+    print(f"Error: {e}")
