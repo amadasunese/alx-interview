18d791a7c263f58216ee3de8c1b29bd817a9e452
Ese Amadasun <amadasunese@gmail.com>
2023-10-20 20:41:46 +0100

    updated

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,49 +2,41 @@
 """
 log parsing
 """
-
 import sys
 import re
 
+# Initialize variables to store metrics
+total_file_size = 0
+status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
+try:
+    for line in sys.stdin:
+        # Parse the line using regular expressions
+        match = re.match(r'^(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)$', line)
+        if match:
+            ip_address, date, status_code, file_size = match.groups()
+            status_code = int(status_code)
+            file_size = int(file_size)
 
+            # Update metrics
+            total_file_size += file_size
+            status_code_counts[status_code] += 1
+            line_count += 1
 
-if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+            # Check if it's time to print statistics
+            if line_count % 10 == 0:
+                print(f"Total file size: {total_file_size}")
+                for code in sorted(status_code_counts.keys()):
+                    if status_code_counts[code] > 0:
+                        print(f"{code}: {status_code_counts[code]}")
 
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
+except KeyboardInterrupt:
+    # Handle CTRL+C interruption by printing statistics
+    print(f"Total file size: {total_file_size}")
+    for code in sorted(status_code_counts.keys()):
+        if status_code_counts[code] > 0:
+            print(f"{code}: {status_code_counts[code}")
 
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
-
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
-    finally:
-        output(log)
+except Exception as e:
+    print(f"Error: {e}")
